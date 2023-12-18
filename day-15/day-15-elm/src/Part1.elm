33c4a66a408ab1e33677359f2d64c2510113dce5
frectonz <fraol0912@gmail.com>
2023-12-18 21:33:48 +0300

    finish day 15 part 1 with `elm`

--- day-15/day-15-elm/src/Part1.elm
+++ day-15/day-15-elm/src/Part1.elm
@@ -1,8 +1,31 @@
 module Part1 exposing (..)
 
-import Html exposing (Html, h1, text)
+import Html exposing (Html, div, h1, p, text)
+import Input
+
+
+hash : String -> Int
+hash str =
+    str
+        |> String.foldl (\c acc -> modBy 256 (acc + Char.toCode c) * 17) 0
+        |> modBy 256
+
+
+hashSum : String -> Int
+hashSum str =
+    str |> String.split "," |> List.map hash |> List.sum
 
 
 main : Html msg
 main =
-    h1 [] [ text "Part 1" ]
+    let
+        answer =
+            hashSum Input.input
+    in
+    div []
+        [ h1 []
+            [ text "Part 1" ]
+        , p
+            []
+            [ answer |> String.fromInt |> text ]
+        ]
